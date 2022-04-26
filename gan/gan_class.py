01bece0ecaef856917bb75a49d0492d83c164aad
kwmt <kouki1212@chiba-u.jp>
2022-04-27 00:01:51 +0900

    pix2pix class copy

--- gan/gan_class.py
+++ gan/gan_class.py
@@ -7,7 +7,7 @@ import torch.nn as nn
 import torch.optim as optim
 from torch.utils.tensorboard import SummaryWriter
 import json
-
+import time
 
 import py_func.gan.gan_model as ganmd
 import numpy as np
@@ -65,6 +65,7 @@ class AlignedDataset(Dataset):
         # 入力画像を一度286x286にリサイズし、その後で256x256にランダムcropする
         list.append(transforms.Resize([load_size, load_size], Image.BICUBIC))
 
+        #torchvision.transformsのRandomCropでできる？
         (x, y) = param['crop_pos']
         crop_size = self.config.crop_size
         list.append(transforms.Lambda(lambda img: img.crop((x, y, x + crop_size, y + crop_size))))
@@ -121,6 +122,8 @@ class GANLoss(nn.Module):
     def __init__(self):
         super().__init__()
 
+        #register_bufferはモデルのパラメータではないが、保存しておきたい値を入れておく領域
+        #モデルを保存して、再度読み込んでも保持される
         self.register_buffer('real_label', torch.tensor(1.0))
         self.register_buffer('fake_label', torch.tensor(0.0))
         # Real/Fake識別の損失を、シグモイド＋バイナリクロスエントロピーで計算
@@ -133,6 +136,168 @@ class GANLoss(nn.Module):
             target_tensor = self.fake_label
 
         return self.loss(prediction, target_tensor.expand_as(prediction))
+
+# Pix2Pixモデルの定義クラス
+# 入力と出力の画像ペア間のマッピングを学習するモデル
+class Pix2Pix():
+    def __init__(self, config):
+        self.config = config
+
+        # 生成器Gのオブジェクト取得とデバイス設定
+        self.netG = ganmd.Generator_Unet().to(self.config.device)
+        # ネットワークの重み初期化
+        self.netG.apply(self.__weights_init)
+        # 生成器Gのモデルファイル読み込み(学習を引き続き行う場合)
+        #map_locationはmodelを保存したデバイスと、ロードするデバイスが違う場合に必要？
+        #strictはデフォルトはTrue, keyの厳密さ
+        if self.config.path_to_generator != None:
+            self.netG.load_state_dict(torch.load(self.config.path_to_generator, map_location=self.config.device_name), strict=False)
+
+        # 識別器Dのオブジェクト取得とデバイス設定
+        self.netD = ganmd.Discriminatorpix2pix().to(self.config.device)
+        # Dのネットワーク初期化
+        self.netD.apply(self.__weights_init)
+        # Dのモデルファイル読み込み(学習を引き続き行う場合)
+        if self.config.path_to_discriminator != None:
+            self.netD.load_state_dict(torch.load(self.config.path_to_discriminator, map_location=self.config.device_name), strict=False)
+
+        # Optimizerの初期化
+        self.optimizerG = optim.Adam(self.netG.parameters(), lr=0.0002, betas=(0.5, 0.999))
+        self.optimizerD = optim.Adam(self.netD.parameters(), lr=0.0002, betas=(0.5, 0.999))
+        # 目的（損失関数)の設定
+        # GAN損失(Adversarial損失)
+        self.criterionGAN = GANLoss().to(self.config.device)
+        # L1損失
+        self.criterionL1 = nn.L1Loss()
+
+        # 学習率のスケジューラ設定
+        self.schedulerG = optim.lr_scheduler.LambdaLR(self.optimizerG, self.__modify_learning_rate)
+        self.schedulerD = optim.lr_scheduler.LambdaLR(self.optimizerD, self.__modify_learning_rate)
+
+        self.training_start_time = time.time()
+
+        self.writer = SummaryWriter(log_dir=config.log_dir)
+
+    def update_learning_rate(self):
+        # 学習率の更新、毎エポック後に呼ばれる
+        self.schedulerG.step()
+        self.schedulerD.step()
+
+    def __modify_learning_rate(self, epoch):
+        # 学習率の計算
+        # 指定の開始epochから、指定の減衰率で線形に減衰させる
+        if self.config.epochs_lr_decay_start < 0:
+            return 1.0
+
+        delta = max(0, epoch - self.config.epochs_lr_decay_start) / float(self.config.epochs_lr_decay)
+        return max(0.0, 1.0 - delta)
+
+    def __weights_init(self, m):
+        # パラメータ初期値の設定
+        classname = m.__class__.__name__
+        if classname.find('Conv') != -1:
+            m.weight.data.normal_(0.0, 0.02)
+        elif classname.find('BatchNorm') != -1:
+            m.weight.data.normal_(1.0, 0.02)
+            m.bias.data.fill_(0)
+
+    def train(self, data, batches_done):
+        # ドメインAのラベル画像とドメインBの正解画像を設定
+        self.realA = data['A'].to(self.config.device)
+        self.realB = data['B'].to(self.config.device)
+
+        # 生成器Gで画像生成
+        fakeB = self.netG(self.realA)
+
+        # Discriminator
+        # 条件画像(A)と生成画像(B)を結合
+        fakeAB = torch.cat((self.realA, fakeB), dim=1)
+        # 識別器Dに生成画像を入力、このときGは更新しないのでdetachして勾配は計算しない
+        pred_fake = self.netD(fakeAB.detach())
+        # 偽物画像を入力したときの識別器DのGAN損失を算出
+        lossD_fake = self.criterionGAN(pred_fake, False)
+
+        # 条件画像(A)と正解画像(B)を結合
+        realAB = torch.cat((self.realA, self.realB), dim=1)
+        # 識別器Dに正解画像を入力
+        pred_real = self.netD(realAB)
+        # 正解画像を入力したときの識別器DのGAN損失を算出
+        lossD_real = self.criterionGAN(pred_real, True)
+
+        # 偽物画像と正解画像のGAN損失の合計に0.5を掛ける
+        lossD = (lossD_fake + lossD_real) * 0.5
+
+        # Dの勾配をゼロに設定
+        self.optimizerD.zero_grad()
+        # Dの逆伝搬を計算
+        lossD.backward()
+        # Dの重みを更新
+        self.optimizerD.step()
+
+        # Generator
+        # 評価フェーズなので勾配は計算しない
+        # 識別器Dに生成画像を入力
+        with torch.no_grad():
+            pred_fake = self.netD(fakeAB)
+
+        # 生成器GのGAN損失を算出
+        lossG_GAN = self.criterionGAN(pred_fake, True)
+        # 生成器GのL1損失を算出
+        lossG_L1 = self.criterionL1(fakeB, self.realB) * self.config.lambda_L1
+
+        # 生成器Gの損失を合計
+        lossG = lossG_GAN + lossG_L1
+
+        # Gの勾配をゼロに設定
+        self.optimizerG.zero_grad()
+        # Gの逆伝搬を計算
+        lossG.backward()
+        # Gの重みを更新
+        self.optimizerG.step()
+
+        # for log
+        self.fakeB = fakeB
+        self.lossG_GAN = lossG_GAN
+        self.lossG_L1 = lossG_L1
+        self.lossG = lossG
+        self.lossD_real = lossD_real
+        self.lossD_fake = lossD_fake
+        self.lossD = lossD
+
+        train_info = {
+            'epoch': epoch, 
+            'batch_num': batch_num,  
+            'lossG_GAN': lossG_GAN.item(),
+            'lossG_L1': lossG_L1.item(),
+            'lossG': lossG.item(),
+            'lossD_real': lossD_real.item(), 
+            'lossD_fake': lossD_fake.item(), 
+            'lossD': lossD.item(), 
+            }
+
+        self.save_loss(train_info, batches_done)
+
+    def save_model(self, epoch):
+        # モデルの保存
+        output_dir = self.config.output_dir
+        torch.save(self.netG.state_dict(), '{}/pix2pix_G_epoch_{}'.format(output_dir, epoch))
+        torch.save(self.netD.state_dict(), '{}/pix2pix_D_epoch_{}'.format(output_dir, epoch))
+
+    def save_image(self, epoch):
+        # 条件画像、生成画像、正解画像を並べて画像を保存
+        output_image = torch.cat([self.realA, self.fakeB, self.realB], dim=3)
+        vutils.save_image(output_image,
+                '{}/pix2pix_epoch_{}.png'.format(self.config.output_dir, epoch),
+                normalize=True)
+
+        self.writer.add_image('image_epoch{}'.format(epoch), self.fakeB[0], epoch)
+
+    def save_loss(self, train_info, batches_done):
+        """
+        lossの保存
+        """
+        for k, v in train_info.items():
+            self.writer.add_scalar(k, v, batches_done)
 
 
 
@@ -376,6 +541,7 @@ class ESRGAN():
 
         torch.save(self.generator.state_dict(), generator_weight_path)
         torch.save(self.discriminator.state_dict(), discriminator_weight_path)
+
 
 
 def save_json(file, save_path, mode):
